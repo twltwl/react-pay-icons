2ca018797012ef8b0ce5646eb4e16f753ce3eff3
Tommy Ljungberg <tommy@nexure.io>
2021-02-15 19:22:38 +0100

    fix docs

--- src/demo.js
+++ src/demo.js
@@ -6,21 +6,47 @@ const element = document.getElementById("root");
 
 const styleProps = {
   width: 100,
-  margin: 10,
   maxHeight: 50,
 };
 
 const flexProps = {
   display: "flex",
   flexWrap: "wrap",
+  alignItems: "stretch",
 };
 
 ReactDOM.render(
-  <div style={{ backgroundColor: "#f1f1f1" }}>
+  <div>
     <div style={flexProps}>
       {Object.keys(Icons).map((key) => {
         const Icon = Icons[key];
-        return <Icon data-name={key} style={styleProps} />;
+        return (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "10px",
+              margin: "10px",
+              backgroundColor: "#f2f7fc",
+              width: "150px",
+            }}
+          >
+            <span
+              style={{
+                fontFamily: "helvetica",
+                fontWeight: "bold",
+                fontSize: "0.8rem",
+                margin: "5px",
+                color: "#002223",
+              }}
+            >
+              {key}
+            </span>
+            <Icon data-name={key} style={styleProps} />
+          </div>
+        );
       })}
     </div>
   </div>,
