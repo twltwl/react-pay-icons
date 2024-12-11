8b290a133ba5d6f4061383024d70c6e1a500a531
Arun Philip <dacodedbeat@gmail.com>
2024-12-11 10:01:16 -0500

    chore(dependencies): update svgr + svgo; regenerate icons

--- svgr.config.js
+++ svgr.config.js
@@ -2,15 +2,18 @@ const path = require("path");
 
 function template(variables, { tpl }) {
   return tpl`
-  ${variables.imports};
-  ${variables.interfaces};
-    const ${variables.componentName} = (${variables.props}) => 
-      React.createElement('div', {className: props.className, style: props.style}, ${variables.jsx})
-    ${variables.exports}
+${variables.imports};
+
+${variables.interfaces};
+
+const ${variables.componentName} = (${variables.props}) => 
+  React.createElement('div', { className: props.className, style: props.style }, ${variables.jsx});
+
+${variables.exports};
   `;
 }
 
 module.exports = {
   template,
-  outDir: path.resolve(__dirname, "src", "Icons"),
+  outDir: path.resolve(__dirname, "lib"),
 };
