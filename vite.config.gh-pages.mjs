0a7b303782bf16c1acdc3f9060a30da657e7a00d
twltwl <tommy@nexure.io>
2025-08-18 17:03:32 +0200

    clean up

--- vite.config.gh-pages.mjs
+++ vite.config.gh-pages.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-pay-icons/", // 👈 required for GH Pages
+  base: "/react-pay-icons/",
   build: {
     outDir: "gh-pages",
   },
